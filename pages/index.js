dfa48c4a498adfb5e5003e3f12d381c7c9c3658b
Yiran Li <yiran.li@hotmail.com>
2020-12-31 22:47:55 -0700

    Set up main page with title and direct button

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,18 @@
 import Head from 'next/head';
+import Link from 'next/link';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <section className={styles.container}>
       <Head>
         <title>What A Joke</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
-    </div>
+      <h1 className={styles.h1}>What A Joke </h1>
+      <Link href='/jokes'>
+        <h2 className={styles.h2}>👉 Ready for laughing? 👈</h2>
+      </Link>
+    </section>
   );
 }
