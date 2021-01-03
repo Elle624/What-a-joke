1eb088757614ba09ed73403e3be6ed98cac61884
Yiran Li <yiran.li@hotmail.com>
2021-01-02 21:22:15 -0700

    Seperate elements from jokes page to seperate files

--- pages/jokes.js
+++ pages/jokes.js
@@ -1,41 +1,5 @@
-import useSWR, { mutate } from 'swr';
-import React, { useState } from 'react';
-import styles from '../styles/Jokes.module.css';
+import DisplayJokes from './components/Jokes';
 
-const Jokes = () => {
-  let [favJokes, setFavJokes] = useState([]);
-  const { data, error } = useSWR('http://api.icndb.com/jokes/random/');
-  const addFavoriteJoke = (id) => {
-    fetch(`http://api.icndb.com/jokes/${id}`)
-      .then((res) => res.json())
-      .then((data) => {
-        if (!favJokes.find((jokeDeets) => jokeDeets.joke === data.value.joke)) {
-          const updateFavJokesList = [...favJokes, data.value];
-          setFavJokes(updateFavJokesList);
-        }
-      });
-  };
-
-  return (
-    <>
-      <section className={styles.jokeContainer}>
-        {error && <p>failed to get you a laugh, please try again!</p>}
-        <p>{data ? data.value.joke : 'Loading...'}</p>
-        <section className={styles.nav}>
-          <button onClick={() => mutate('http://api.icndb.com/jokes/random/')}>
-            🤦‍♀️❌
-          </button>
-          <button onClick={() => addFavoriteJoke(data.value.id)}>😹🉑</button>
-        </section>
-      </section>
-      <section className={styles.displayFavJokes}>
-        {favJokes.map((jokeDetails) => (
-          <section key={jokeDetails.id} className={styles.favJokeCard}>
-            <p>{jokeDetails.joke}</p>
-          </section>
-        ))}
-      </section>
-    </>
-  );
-};
-export default Jokes;
+export default function Jokes() {
+  return <DisplayJokes />;
+}
