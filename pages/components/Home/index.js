d59bdb7d27562897723b011f06dd68652d589c57
Yiran Li <yiran.li@hotmail.com>
2021-01-02 21:17:17 -0700

    Seperate home page elements to seperate file

---
+++ pages/components/Home/index.js
@@ -0,0 +1,20 @@
+import Head from 'next/head';
+import Link from 'next/link';
+import styles from '../../../styles/Home.module.css';
+
+const Home = () => {
+  return (
+    <section className={styles.container}>
+      <Head>
+        <title>What A Joke</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <h1 className={styles.h1}>What A Joke </h1>
+      <Link href='/jokes'>
+        <h2 className={styles.h2}>👉 Ready for laughing? 👈</h2>
+      </Link>
+    </section>
+  );
+};
+
+export default Home;
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,22 @@
-import Head from 'next/head';
-import Link from 'next/link';
-import styles from '../styles/Home.module.css';
+// import Head from 'next/head';
+// import Link from 'next/link';
+// import styles from '../styles/Home.module.css';
 
-export default function Home() {
-  return (
-    <section className={styles.container}>
-      <Head>
-        <title>What A Joke</title>
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
-      <h1 className={styles.h1}>What A Joke </h1>
-      <Link href='/jokes'>
-        <h2 className={styles.h2}>👉 Ready for laughing? 👈</h2>
-      </Link>
-    </section>
-  );
+// export default function Home() {
+//   return (
+//     <section className={styles.container}>
+//       <Head>
+//         <title>What A Joke</title>
+//         <link rel='icon' href='/favicon.ico' />
+//       </Head>
+//       <h1 className={styles.h1}>What A Joke </h1>
+//       <Link href='/jokes'>
+//         <h2 className={styles.h2}>👉 Ready for laughing? 👈</h2>
+//       </Link>
+//     </section>
+//   );
+// }
+import Home from './components/Home';
+export default function App() {
+  return <Home />;
 }
