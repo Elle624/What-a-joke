9239c0effd4888352c99c406db9833de61d351d0
Yiran Li <yiran.li@hotmail.com>
2021-01-03 22:47:44 -0700

    Clean up space

--- cypress/integration/jokes.js
+++ cypress/integration/jokes.js
@@ -1,5 +1,6 @@
 describe('Jokes page', () => {
-  beforeEach(() => {
+  
+  it('Should display a joke and emoji buttons', () => {
     cy.visit('http://localhost:3000');
     cy.intercept(
       {
@@ -17,39 +18,71 @@ describe('Jokes page', () => {
       }
     );
     cy.get('h2').click();
-  });
 
-  it('Should display a joke and emoji buttons', () => {
     cy.contains('This is a test joke').get('button').should('have.length', 2);
   });
 
-  it.only('Should display a random joke if click dislike button', () => {
+  it('Should display a random joke if click dislike button', () => {
+    cy.visit('http://localhost:3000/jokes', {
+      onBeforeLoad(win) {
+        cy.stub(win, 'fetch')
+          .withArgs('http://api.icndb.com/jokes/random')
+          .resolves(
+            Cypress.Promise.resolve({
+              ok: true,
+              json: () => ({
+                value: {
+                  id: 2,
+                  joke: 'This is a test joke'
+                }
+              })
+            }).delay(2000)
+          );
+      }
+    });
+
+    cy.get('button').first().click();
+    cy.contains('This is a test joke');
+
+    cy.visit('http://localhost:3000/jokes', {
+      onBeforeLoad(win) {
+        cy.stub(win, 'fetch')
+          .withArgs('http://api.icndb.com/jokes/random')
+          .resolves(
+            Cypress.Promise.resolve({
+              ok: true,
+              json: () => ({
+                value: {
+                  id: 24,
+                  joke: 'A lemon is walking on the street'
+                }
+              })
+            }).delay(2000)
+          );
+      }
+    });
+    cy.contains('A lemon is walking on the street');
+  });
+
+  it('Should display favorite joke if click favorite button', () => {
+    cy.visit('http://localhost:3000');
     cy.intercept(
       {
         method: 'GET',
         url: 'http://api.icndb.com/jokes/random'
       },
       {
-        statusCode: 202,
+        statusCode: 201,
         body: {
           value: {
-            id: 24,
-            joke: 'A lemon is walking on the street'
+            id: 2,
+            joke: 'This is a test joke'
           }
         }
       }
     );
-    // cy.request('GET', 'http://api.icndb.com/jokes/random', {
-    //   value: {
-    //     id: 24,
-    //     joke: 'A lemon is walking on the street'
-    //   }
-    // });
-    cy.get('button').first().click();
-    cy.contains('A lemon is walking on the street');
-  });
+    cy.get('h2').click();
 
-  it('Should display favorite joke if click favorite button', () => {
     cy.intercept(
       {
         method: 'GET',
@@ -65,6 +98,7 @@ describe('Jokes page', () => {
         }
       }
     );
+
     cy.get('button').first().next().click();
     cy.get('p').should('have.length', 2);
   });
